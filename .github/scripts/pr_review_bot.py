175d23363c6c86a743acdee4286b37dda8d41286
Muizz Lateef <Lateefmuizz@gmail.com>
2023-09-05 15:11:38 +0100

    Update pr_review_bot.py

--- .github/scripts/pr_review_bot.py
+++ .github/scripts/pr_review_bot.py
@@ -1,4 +1,3 @@
-
 import os
 import json
 import openai
@@ -20,22 +19,35 @@ def post_comment(pr_id, comment):
     if response.status_code != 201:
         print(f"Failed to post comment: {response.text}")
 
-def review_code(pr_id):
-    # TODO: Extract changes from the PR
-    code_snippet = "PLACEHOLDER_FOR_CODE_FROM_PR"
+def get_pr_files(pr_id):
+    url = f"https://api.github.com/repos/{os.environ['GITHUB_REPOSITORY']}/pulls/{pr_id}/files"
+    headers = {
+        "Authorization": f"token {os.environ['GITHUB_TOKEN']}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Failed to get PR files: {response.text}")
+        return []
 
-    response = openai.Completion.create(
-      engine="davinci",
-      prompt=f"Review the following code snippet:\n\n{code_snippet}\n",
-      max_tokens=150
-    )
-    
-    review = response.choices[0].text.strip()
-    post_comment(pr_id, review)
+def review_code(pr_id):
+    files = get_pr_files(pr_id)
+    for file in files:
+        code_snippet = file.get('patch', 'No changes available')
+
+        response = openai.Completion.create(
+          engine="davinci",
+          prompt=f"Review the following code snippet:\n\n{code_snippet}\n",
+          max_tokens=150
+        )
+        
+        review = f"Review for file `{file['filename']}`:\n\n" + response.choices[0].text.strip()
+        post_comment(pr_id, review)
 
 if __name__ == "__main__":
     with open(os.environ["GITHUB_EVENT_PATH"], "r") as f:
         event_data = json.load(f)
         pr_id = event_data["pull_request"]["number"]
-
     review_code(pr_id)
