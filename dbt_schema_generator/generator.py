effda0614da250f78486dba6f7750bb92714f0ca
Muizz Lateef <muizz@Muizzs-MacBook-Pro.local>
2023-09-05 16:24:19 +0100

    added

--- dbt_schema_generator/generator.py
+++ dbt_schema_generator/generator.py
@@ -67,7 +67,7 @@ def get_path_models(manifest_data: Dict, path_name: str) -> Dict:
 
 
 def create_schema(models: Dict) -> str:
-    """Creates the schema YAML content  the ."""
+    """Creates the schema YAML   the ."""
     schema = "version: 2\n\nmodels:\n\n"
     for model in models.values():
         model_dict = {
